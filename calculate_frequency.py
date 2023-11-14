54e6018c07acbd92726b4d98522aad31f4660414
sharmistharanit <143737948+sharmistharanit@users.noreply.github.com>
2023-11-13 20:42:02 -0500

    Update calculate_frequency.py

--- calculate_frequency.py
+++ calculate_frequency.py
@@ -1,3 +1,6 @@
 import numpy as np
 import pandas as pd
-
+def calculate_frequencies(fft_output, sampling_rate):
+    N = len(fft_output)
+    frequencies = np.fft.fftfreq(N, d=1/sampling_rate)
+    return frequencies
