71a6e3529285b7a1c4f632f4d809a830e71cfd52
Mosaddeq Hossain <66480280+Mosaddeq107@users.noreply.github.com>
2023-11-13 23:44:06 -0500

    Update calculate_frequency.py

--- calculate_frequency.py
+++ calculate_frequency.py
@@ -1,9 +1,12 @@
 import numpy as np
-#import pandas as pd
+
+
 def calculate_frequencies(fft_output, sampling_rate):
     N = len(fft_output)
     frequencies = np.fft.fftfreq(N, d=1/sampling_rate)
     return frequencies
+
+
 def find_peak_frequency(fft_output, sampling_rate):
     frequencies = calculate_frequencies(fft_output, sampling_rate)
     positive_frequencies = frequencies[:len(frequencies)//2]
